9d405d5cced893267ef4d9fa713b25fb8fc7185e
Askhat Tuleshov <askhattk@ya.ru>
2014-06-04 18:43:10 +0600

    greeter class added

---
+++ src/main/java/com/epam/at/helloworld/Greeter.java
@@ -0,0 +1,8 @@
+package com.epam.at.helloworld;
+
+public class Greeter {
+    public static void main(String[] args) {
+        Greeter greet = new Greeter();
+        System.out.println(greet.getClass());
+    }
+}
